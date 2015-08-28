126bbc363a50d1dc65c251686290c6176ec39fd2
jan <jlhellmich@gmail.com>
2015-08-27 17:13:26 -0700

    Add automatic server setup and file watch/rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 
 var webpack = require('gulp-webpack');
+var shell = require('gulp-shell');
 
 
 gulp.task('build', function() {
@@ -8,3 +9,13 @@ gulp.task('build', function() {
     .pipe(webpack( require('./webpack.config.js') ))
     .pipe(gulp.dest('client/dist/'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('client/src/*.js', ['build'])
+});
+
+gulp.task('start-server', shell.task([
+  'node app.js'
+]));
+
+gulp.task('dev', ['build', 'watch', 'start-server']);
