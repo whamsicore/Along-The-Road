a5af6c8a84dc0a32ee607eddbcd6d484820f4e85
jan <jlhellmich@gmail.com>
2015-08-28 10:42:57 -0700

    Add autocomplete input fields and pass location data to next view

--- client/src/mapView.js
+++ client/src/mapView.js
@@ -4,19 +4,23 @@ This component is the map view. It shows the user the possible routes he/she can
 
 var React = require('react');
 
-var Router = require('react-router');
-
 var MapView = React.createClass({
+  /*
+   proptypes provide a way of checking the passed in properties.
+   They throw a warning in the console if not provided correctly.
+   It has no effect on the execution of the program
+  */
   propTypes: {
-    origin: React.PropTypes.string.isRequired,
-    destination: React.PropTypes.string.isRequired,
-    setPath: React.PropTypes.func.isRequired,
+    origin: React.PropTypes.object.isRequired,
+    destination: React.PropTypes.object.isRequired,
+    setOrigin: React.PropTypes.func.isRequired,
+    setDestination: React.PropTypes.func.isRequired,
   },
 
   render () {
     return (
       <div>
-        Welcome to the MapView: You requested to go from {this.props.origin} to {this.props.destination}!
+        Welcome to the MapView: You requested to go from {this.props.origin.formatted_address} to {this.props.destination.formatted_address}!
       </div>
     )
   }
