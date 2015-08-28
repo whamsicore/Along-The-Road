b45cea97092f6d5470d73fceb5f7c3e0e4a526b6
jan <jlhellmich@gmail.com>
2015-08-27 20:13:16 -0700

    Add origin and location state to app view

--- client/src/homeView.js
+++ client/src/homeView.js
@@ -8,11 +8,23 @@ var Router = require('react-router');
 var Link = Router.Link;
 
 var HomeView = React.createClass({
+  propTypes: {
+    origin: React.PropTypes.string.isRequired,
+    destination: React.PropTypes.string.isRequired,
+    setPath: React.PropTypes.func.isRequired,
+  },
+
   render () {
     return (
       <div>
-        Welcome to the HomeView
-        <Link to="map">Go to MapView</Link>
+        Pick your Route
+        <form>
+          <input type="text" name="origin" placeholder="origin" />
+          <input type="text" name="destination" placeholder="destination" />
+          <button onClick={this.props.setPath.bind(null, 'SF', 'LA')}>
+            <Link to="map" >Submit</Link>
+          </button>
+        </form>
       </div>
     )
   }
--- client/src/mapView.js
+++ client/src/mapView.js
@@ -7,10 +7,16 @@ var React = require('react');
 var Router = require('react-router');
 
 var MapView = React.createClass({
+  propTypes: {
+    origin: React.PropTypes.string.isRequired,
+    destination: React.PropTypes.string.isRequired,
+    setPath: React.PropTypes.func.isRequired,
+  },
+
   render () {
     return (
       <div>
-        Welcome to the MapView
+        Welcome to the MapView: You requested to go from {this.props.origin} to {this.props.destination}!
       </div>
     )
   }
