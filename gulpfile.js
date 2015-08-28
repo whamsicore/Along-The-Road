654bc37cba8864832316c8ca6475303ea0c0cf32
jan <jlhellmich@gmail.com>
2015-08-27 17:23:48 -0700

    Add automatic opening of app entry point

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,31 @@ var gulp = require('gulp');
 
 var webpack = require('gulp-webpack');
 var shell = require('gulp-shell');
+var open = require('gulp-open');
 
-
+// Bundles all files and runs the webpack loaders specified in webpack.config.js
 gulp.task('build', function() {
   return gulp.src('client/src/app.js')
     .pipe(webpack( require('./webpack.config.js') ))
     .pipe(gulp.dest('client/dist/'));
 });
 
+// Watches for file changes and rebuilds
 gulp.task('watch', function() {
   gulp.watch('client/src/*.js', ['build'])
 });
 
+// Starts the node server to serve static assets
 gulp.task('start-server', shell.task([
   'node app.js'
 ]));
 
-gulp.task('dev', ['build', 'watch', 'start-server']);
+// opens the entry point of the app in default browser
+gulp.task('open-browser', function(){
+  setTimeout(function() {
+    gulp.src(__filename)
+      .pipe(open({uri: 'http://localhost:3000/'}));
+  }, 2000);
+});
+
+gulp.task('dev', ['build', 'watch', 'start-server', 'open-browser']);
