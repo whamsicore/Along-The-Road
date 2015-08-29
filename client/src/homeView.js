e60a3352e2029f846dca7418fd93e34c14013d5b
Whamsicore <whamsicore@whambook.local>
2015-08-29 01:18:06 -0700

    (feat) Experimenting with bootstrap in homepage

--- client/src/homeView.js
+++ client/src/homeView.js
@@ -59,23 +59,60 @@ var HomeView = React.createClass({
   render () {
     return (
       <div className='container'>
-        <p>Plan your Trip</p>
-          <div className = 'row'>
-            <div className = 'col-sm-12'>
-              <TextField className= 'center-block' id="origin" placeholder='' floatingLabelText="Start Location" hintText="Enter a starting location"/>
+        <div className='jumbotron'>
+          <div className='container panel'>
+            <div className = 'row'>
+              <div className = 'col-sm-12'>
+                <h2>Plan your Trip</h2>
+              </div>
             </div>
-          </div>
-          <div className = 'row'>
-            <div className = 'col-sm-12'>
-              <TextField id="destination" placeholder='' floatingLabelText="End Location" hintText="Enter a destination"/>
-            </div>
-          </div>
 
+            <div className = 'row'>
+              <div className = 'col-sm-6'>
+                <div className = 'container'>
+                  <div className = 'row'>
+                    <div className = 'col-sm-12'>
+                      <TextField className= 'center-block' id="origin" placeholder='' floatingLabelText="Start Location" hintText="Enter a starting location"/>
+                    </div>
+                  </div>
+                  <div className = 'row'>
+                    <div className = 'col-sm-12'>
+                      <TextField id="destination" placeholder='' floatingLabelText="End Location" hintText="Enter a destination"/>
+                    </div>
+                  </div>
+                  <div className = 'row'>
+                    <div className = 'col-sm-12'>
+                      <RaisedButton label="Submit" linkButton="true" params={{origin: this.state.origin, destination: this.state.destination }} containerElement={<Link to="map"/>}/>
+                    </div>
+                  </div>
+
+                </div> {/* container */}
+
+              </div> {/* col-sm-6 */}
+
+              <div className = 'col-sm-6'>
+                <div className = 'row'>
+                  <div className = 'col-sm-12'>
+                    <p>
+                      {"Enter a route and find all the best places to eat along the way!"}
+                    </p>
+                  </div>
+                </div> {/* row */}
+              </div> {/* col-sm-6 */}
+            </div> {/* row */}
+          </div> {/* container */}
+        </div> {/* jumbotron */}
+        <div className='container'>
           <div className = 'row'>
             <div className = 'col-sm-12'>
-              <RaisedButton label="Submit" linkButton="true" params={{origin: this.state.origin, destination: this.state.destination }} containerElement={<Link to="map"/>}/>
+              <h1> Get The App (coming soon...) </h1>
+
+              <h3>
+                {"Stumptown butcher four loko trust fund banh mi, mlkshk ugh 8-bit cred. Fanny pack cornhole bitters jean shorts, drinking vinegar fap Intelligentsia disrupt freegan Thundercats sartorial lumbersexual brunch beard blog. Shoreditch Austin health goth wolf stumptown, fashion axe vinyl photo booth hashtag cronut. Selvage plaid Williamsburg iPhone, umami hashtag blog stumptown fap. Cred Schlitz pork belly, kogi gastropub crucifix lomo McSweeney's actually disrupt aesthetic narwhal. Try-hard tote bag scenester butcher, keytar."}
+              </h3>
             </div>
           </div>
+        </div> {/* jumbotron */}
       </div>
     )
   }
