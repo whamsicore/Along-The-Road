2bb811851c78d024d3cd1b17d8708652198e372c
Whamsicore <whamsicore@whambook.local>
2015-09-01 11:57:00 -0700

    add listView to display restaurants:

---
+++ client/src/listView.js
@@ -0,0 +1,27 @@
+/*
+This view shows the details of the possible routes from origin to destination
+*/
+
+var React = require('react');
+
+var ListView = React.createClass({
+  propTypes: {
+    routingBoxes: React.PropTypes.array.isRequired
+  },
+
+  render () {
+    var listDetails = this.props.routingBoxes.map(function(box, index) {
+      return (
+        <div key={index}>
+          Restaurant {index}
+        </div>
+      )
+    });
+
+    return (
+      <div> {listDetails} </div>
+    );
+  }
+});
+
+module.exports = ListView;
