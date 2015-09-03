63e6dce22457bfe372766a37f0be5cab8e5d2299
Whamsicore <whamsicore@whambook.local>
2015-09-03 15:17:45 -0700

    (feat) bug free before fully implementing foursquare

--- client/src/listView.js
+++ client/src/listView.js
@@ -6,13 +6,60 @@ var React = require('react');
 
 var ListView = React.createClass({
   propTypes: {
-    wayPoints: React.PropTypes.array.isRequired
+    currentRoute: React.PropTypes.object.isRequired
   },
+  defaultOptions: {
+    // fourSquare_url: "https://api.foursquare.com/v2/venues/search?client_id=LFDSJGGT42FEYM4KFGYR2ETFQZDEMTAVN0KQ0NHBLUXJU4UB&client_secret=YVKQEEBYGFUAMSNFRFEB1MJAEYRXHVBWOL35KFA51ITJBWEE&v=20150902"
+  },
+  //
   shouldComponentUpdate (nextProps, nextState){
-    console.log("TEST inside shouldComponentUpdate() nextProps=", nextProps);
-    // console.log("TEST inside shouldComponentUpdate() nextState=", nextState);
-    return true;
+    console.log("TEST ---> inside shouldComponentUpdate() nextProps = ", nextProps);
+
+    // var component = this;
+    var currentRoute = nextProps.currentRoute;
+    var wayPoints = currentRoute.wayPoints;
+    // var data;
+    if(wayPoints.length > 0){
+      if(currentRoute.results){
+        return currentRoute.results;
+      }else{
+        this.queryFourSquare(wayPoints);
+      } //if
+
+     // console.log("wayPoints.length is positive");
+     return true;
+    }
+
+    return false; //tells React to continue with render()
   },
+  //queries fourSquare api to get new results.
+  //save results to the current route and updates the parent (mapView)
+  //re-render results onto the page by updating state variable.
+  queryFourSquare (wayPoints){
+    console.log('TEST---->this.getFourSquare() wayPoints = ', wayPoints);
+    var results = [];
+
+
+    // for(var i=0; i<wayPoints.length; i++){
+    //   var point = wayPoints[i];
+    //   var ll = "&ll="+point.+","+;
+    //   $.ajax({
+    //     url: this.defaultOptions.fourSquare_url+ll,
+    //     method: "GET",
+    //     success: function(data){
+    //       console.log("TEST -------> fourSquare success, body=", data);
+
+    //     },
+    //     error: function(error){
+    //       console.log("TEST -------> fourSquare error, error=", error);
+
+    //     }
+    //     // dataType: dataType
+    //   }); //ajax()
+
+    // } //for
+
+  }, // queryFourSquare()
   // componentWillUpdate (){
   //   console.log("TEST -----> componentWillUpdate");
   // },
@@ -21,7 +68,7 @@ var ListView = React.createClass({
 
   // },
   render () {
-    var listDetails = this.props.wayPoints.map(function(box, index) {
+    var listDetails = this.props.currentRoute.wayPoints.map(function(box, index) {
       return (
         <div key={index}>
           Restaurant {index}
