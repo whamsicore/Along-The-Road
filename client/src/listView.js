26fe17f55a733084a1e2418d8a31d2f03f397bea
Sophia Bong <sophiawbong@gmail.com>
2015-09-05 16:33:40 -0700

    (feat) Add Material UI and Bootstrap styling to VenueView

--- client/src/listView.js
+++ client/src/listView.js
@@ -82,16 +82,7 @@ var ListView = React.createClass({
     } //for
 
   }, // queryFourSquare()
-  /************* Sample Venue **********/
-  // {"id":"4cfae4ecdccef04d382cb89c",
-  // "name":"New Ringgold Fire Co",
-  // "contact":{},
-  // "location":{"lat":40.68752093923294,"lng":-75.99987570958817,"distance":1389,"postalCode":"17960","cc":"US","city":"New Ringgold","state":"PA","country":"United States","formattedAddress":["New Ringgold, PA 17960","United States"]},
-  // "categories":[{"id":"4bf58dd8d48988d116941735","name":"Bar","pluralName":"Bars","shortName":"Bar","icon":{"prefix":"https:\/\/ss3.4sqi.net\/img\/categories_v2\/nightlife\/pub_","suffix":".png"},"primary":true}],
-  // "verified":false,
-  // "stats":{"checkinsCount":101,"usersCount":55,"tipCount":0},
-  // "allowMenuUrlEdit":true,"specials":{"count":0,"items":[]},"hereNow":{"count":0,"summary":"Nobody here","groups":[]},"referralId":"v-1441238271"}
-  /************* Sample Venue **********/
+
   render () {
     var listDetails = this.props.currentRoute.results.map(function(venue, index) {
       return (
--- client/src/venueView.js
+++ client/src/venueView.js
@@ -4,9 +4,24 @@ This view shows the details of each venue
 
 var React = require('react');
 
+var mui = require('material-ui');
+var ThemeManager = new mui.Styles.ThemeManager();
+var {Card, CardHeader, CardMedia, CardActions, CardText, Avatar, CardTitle} = mui;
+
+
 var VenueView = React.createClass({
   propTypes: {
     venue: React.PropTypes.object.isRequired
+  },
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  getChildContext: function() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
   },
 
   render () {
@@ -14,11 +29,7 @@ var VenueView = React.createClass({
 
     if (categories) {
       var categoryList = categories.map(function(category, index) {
-        return (
-          <div class="category">
-            {category.shortName}
-          </div>
-        )
+        return category.shortName;
       });
     }
 
@@ -27,17 +38,71 @@ var VenueView = React.createClass({
     }
 
 
+    // <img src={photoUrl ? photoUrl : "https://foursquare.com/img/categories/food/default_64.png"}/>
+    // {name}
+    // {location.formattedAddress[0] ? location.formattedAddress[0] : null}
+    // {Math.round(location.distance/1000*.621*10)/10} mi.
+    // {rating ? rating : null}
+    // {price && price.message ? price.message : null}
+    // {categoryList ? categoryList : null}
+    // {hours && hours.status ? hours.status : null}
+
+    // <Card>
+    //   <CardHeader
+    //     title={name}
+    //     subtitle={categoryList ? categoryList : null} />
+    //   <CardMedia>
+    //     <img src={photoUrl ? photoUrl : "https://foursquare.com/img/categories/food/default_64.png"}/>
+    //   </CardMedia>
+    //   <CardActions>
+    //     <FlatButton label="Show Details" />
+    //   </CardActions>
+    //   <CardText>
+    //     {location.formattedAddress[0] ? location.formattedAddress[0] : null}
+    //     {Math.round(location.distance/1000*.621*10)/10} mi.
+    //     {rating ? rating : null}
+    //     {price && price.message ? price.message : null}
+    //     {hours && hours.status ? hours.status : null}
+    //   </CardText>
+    // </Card>
+
+    var avatar = (
+      <Avatar
+        src={photoUrl ? photoUrl : "https://foursquare.com/img/categories/food/default_64.png"}
+        size={70}>
+      </Avatar>
+    );
+
+    var msgToDollarSigns = {
+      Cheap: "$",
+      Moderate: "$$",
+      Expensive: "$$$"
+    };
+
+
+    var categoryText = categoryList ? categoryList.join("/") : "N/A";
+    var priceText = price && price.message ? msgToDollarSigns[price.message] : "N/A";
+    var ratingText = rating ? rating : "N/A";
+    var distanceText = Math.round(location.distance/1000*.621*10)/10 + " mi.";
+
+    var subtitleInfo = [categoryText, priceText, ratingText, distanceText];
+
     return (
-      <div class="venue">
-        <img src={photoUrl ? photoUrl : "https://foursquare.com/img/categories/food/default_64.png"}/>
-        {name}
-        {location.formattedAddress[0] ? location.formattedAddress[0] : null}
-        {Math.round(location.distance/1000*.621*10)/10} mi.
-        {rating ? rating : null}
-        {price && price.message ? price.message : null}
-        {categoryList ? categoryList : null}
-        {hours && hours.status ? hours.status : null}
-      </div>
+      <Card>
+        <div className="col-xs-1" >
+          {avatar}
+        </div>
+        <div className="col-xs-11">
+          <CardTitle
+            title={name}
+            subtitle={subtitleInfo.join(" ● ")} />
+          <CardText>
+            {location.formattedAddress[0] ? location.formattedAddress[0] : null}
+            {hours && hours.status ? " ● " + hours.status : null}
+          </CardText>
+        </div>
+
+      </Card>
     );
   }
 });
