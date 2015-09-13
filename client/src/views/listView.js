9e47697a9663aa9ce0a6c3221b2ef78787d24136
Whamsicore <whamsicore@whambook.local>
2015-09-13 15:38:42 -0700

    Big refactor, stage 1

--- client/src/views/listView.js
+++ client/src/views/listView.js
@@ -13,100 +13,32 @@ var VenueStore = require('../stores/VenueStore');
 var ListView = React.createClass({
 
   propTypes: {
-    currentRoute: React.PropTypes.object.isRequired,
-    searchRadius: React.PropTypes.number.isRequired
+    
   },
 
   defaultOptions: {
 
-    fourSquare_url: "https://api.foursquare.com/v2/venues/explore?client_id=ELLZUH013LMEXWRWGBOSNBTXE3NV02IUUO3ZFPVFFSZYLA30&client_secret=U2EQ1N1J4EAG4XH4QO4HCZTGM3FCWDLXU2WJ0OPTD2Q3YUKF&v=20150902",
-
-    foodCategory_url: "&categoryId=4d4b7105d754a06374d81259",
-    limit_url: "&limit=10",
-    photos_url: "&venuePhotos=1",
-    category_url: "&section=food",
-    distance_url: "&sortByDistance=0"
+    
   },
   componentDidMount () {
-    QueryStore.addChangeListener(this._onChange)
+    console.log("listView -----> componentDidMount()");
+
+    // QueryStore.addChangeListener(this._onChange)
   },
 
   componentDidUpdate(prevProps, prevState) {
     // if (this.props.currentRoute.wayPoints.length && !this.props.currentRoute.results.length && QueryStore.prevWaypoints()==21) {
     //   this.queryFourSquare(1, 21);
-    // }
-  },
-
-  //Gets the previous number of waypoints and the new number to be querried
-  _onChange () {
-    var waypoints = Store.getWaypoints();
-    // if (this.props.currentRoute.wayPoints.length && !this.props.currentRoute.results.length) {
-    this.queryFourSquare(waypoints);
     // } 
   },
 
-  //queries fourSquare api to get new results.
-  //save results to the current route and updates the parent (mapView)
-  //re-render results onto the page by updating state variable.
-  queryFourSquare (wayPoints) {
-    var results = {}; //test against duplicates
-    var component = this;
-
-
-    for(var i = 0; i < wayPoints.length ; i++) {
-      var point = wayPoints[i];
-      var ll = "&ll="+point.G+","+point.K;
-      var radius_url = "&radius="+this.props.searchRadius*1000;
-
-      //These two properties ensure that the data is only displayed once all of the requests have returned
-      //It is important for the speed of the app and ensuring that everything works
-      var sortingPoint = wayPoints.length%20-1;
-      var count = 1;
-
-      var {fourSquare_url, foodCategory_url, category_url, limit_url, photos_url, distance_url} = this.defaultOptions;
-      $.ajax({
-        url: fourSquare_url+ll+category_url+radius_url+limit_url+photos_url+distance_url,
-        method: "GET",
-        success: function(data){
-          count++;
-          var newResults = {};
-          var newResultsArray = [];
-          var venues = data.response.groups[0].items;
-
-          var point = this;
-          for (var i = 0; i < venues.length; i++) {
-            var venue = venues[i].venue;
-            venue.point = point;
-
-            venue.totalDistance = venue.location.distance + venue.point.distance; // in meters
-            if (!results[venue.id]) { // if
-                newResults[venue.id] = venue;
-            } else {
-              if (results[venue.id].location.distance > venue.location.distance) {
-                newResults[venue.id] = venue;
-              }
-            } //if
-          } //for
-
-
-          for (var id in newResults) {
-            newResultsArray.push(newResults[id]);
-          }
-
-          Actions.addVenues(newResultsArray);
-
-          if(count >= sortingPoint){
-            Actions.sortVenues();
-          }
-        }.bind(point), //success()
-        error: function(error){
-          console.log("TEST -------> fourSquare error, error=", error);
-        }
-        // dataType: dataType
-      }); //ajax()
-    } //for
-
-  }, // queryFourSquare()
+  //Gets the previous number of waypoints and the new number to be querried
+  // _onChange () {
+  //   var waypoints = QueryStore.getWaypoints();
+  //   // if (this.props.currentRoute.wayPoints.length && !this.props.currentRoute.results.length) {
+  //   this.queryFourSquare(waypoints);
+  //   // } 
+  // },
 
   render () {
     var component = this;
--- client/src/views/mapView.js
+++ client/src/views/mapView.js
@@ -11,18 +11,107 @@ var MapView = React.createClass({
   },
   componentDidMount () {
     console.log("MapView ---> inside componentDidMount");
+    
     // QueryStore.addChangeListener(this._onChange)
+
   },
 
   componentDidUpdate(prevProps, prevState) {
-    console.log("MapView ---> inside componentDidUpdate");
+    console.log("MapView ---> inside componentDidUpdate ");
 
   },
 
   //Gets the previous number of waypoints and the new number to be querried
   _onChange () {
-
+    // this.clearMapMarkers(); 
+    // this.updateMapMarkers(); //results
   },
+
+
+  // Print new markers
+  updateMapMarkers(results){
+    // set to new prop
+    results = this.props.results; 
+
+    var map = this.state.map;
+    var markers = this.state.markers; //array of
+    var component = this;
+
+    results.forEach(function(venue, index){
+      var {lng, lat} = venue.location;
+
+      //create new marker
+      if(!markers[venue.id]){ //
+        var position = new google.maps.LatLng(lat, lng);
+
+        var marker = new google.maps.Marker({
+          position: position,
+        });
+
+        // create custom infowindow
+        // NOTE: we can also add rating color to decorate marker
+        var infowindow = new google.maps.InfoWindow({
+          content: venue.name + "<br> Rating: "+venue.rating
+        });
+
+        var markerIsActive = false;
+
+        //create event listener to open info window
+        google.maps.event.addListener(marker, 'mouseover', function() {
+          if(!markerIsActive){
+            infowindow.open(map, this);
+          }
+        }); //mouseover
+
+        // create event listener to close info window
+        google.maps.event.addListener(marker, 'mouseout', function() {
+          if(!markerIsActive){
+            infowindow.close();
+          }
+        }); //mouseout
+
+        // create event listener to close info window
+        google.maps.event.addListener(marker, 'click', function() {
+          if(markerIsActive){ // deactivate this marker 
+            markerIsActive = false; 
+            infowindow.close(); 
+
+
+          }else if(!markerIsActive){ //activate this marker
+            markerIsActive = true; 
+            if(window.activeInfoWindow){
+              window.activeInfoWindow.close(); //close previous
+            }
+            window.activeInfoWindow = infowindow; //update current;  
+          } //if
+        }); //mouseout
+
+        google.maps.event.addListener(marker, 'dblclick', function() {
+          component.openFourSquare(venue); //load new page
+          
+        });
+
+        //show map marker
+        marker.setMap(map);
+        // add current marker to state
+        component.state.markers[venue.id] = marker;
+        // component.state.markers.push(marker);
+      } //if
+
+      //display markers
+    }); //forEach
+  }, //updateMapMarkers()
+
+  // clear map markers
+  clearMapMarkers (markers){
+    this.state.markers = {};
+
+    for(var key in markers){
+      var marker = markers[key];
+      marker.setMap(null);
+    } //for
+  }, //clearMapMarkers
+
 
   render () {
     // var component = this;
